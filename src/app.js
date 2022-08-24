def870b8abcd6b98fcbf9d1ecd901705dfd3c06b
rapeeprn <rapeepornk63@nu.ac.th>
2022-08-24 22:25:37 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,3 +18,7 @@ let port = 8081
 app.listen(port, function () {
  console.log('server running on ' + port)
 })
+let port = 8081
+app.listen(port, function () {
+ console.log('server running on ' + port)
+})
